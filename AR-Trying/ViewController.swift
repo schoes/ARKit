fa509532f3b20f6679fdc47737b99861b7780364
Sven Schoeni <sven.schoeni@gmail.com>
2019-02-13 13:15:11 +0100

    Initial Commit

---
+++ AR-Trying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR-Trying
+//
+//  Created by Sven Schöni on 13.02.19.
+//  Copyright © 2019 Sven Schoeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
